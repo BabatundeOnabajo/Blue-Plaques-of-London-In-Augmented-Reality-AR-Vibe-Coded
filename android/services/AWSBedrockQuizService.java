c466cc98581fd868dd04a654740abc67c29d38c9
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2026-01-28 11:14:27 +0000

    Create AWSBedrockQuizService.java
    Added an empty file for the AWS Bedrock Service.

---
+++ android/services/AWSBedrockQuizService.java
@@ -0,0 +1,7 @@
+package com.example.newmediawritingprizesubmission2.services;
+
+public class AWSBedrockQuizService {
+
+// This is an empty file. This is deliberate and may be changed or amended later. 
+
+}
